1c1da88398f8e52c1608cb3fcd98967c1a1d86b2
vandevsysX64 <vandevdev.com@gmail.com>
2026-03-03 13:57:47 +0400

    Update URL to version 1.1 for Lol formula

--- Formula/lol.rb
+++ Formula/lol.rb
@@ -1,7 +1,7 @@
 class Lol < Formula
   desc "Rainbow fortune CLI"
   homepage "https://github.com/vandevsysX64/homebrew-lol"
-  url "https://github.com/vandevsysX64/homebrew-lol/archive/refs/tags/v1.0.tar.gz"
+  url "https://github.com/vandevsysX64/homebrew-lol/archive/refs/tags/v1.1.tar.gz"
   sha256 "cbaff96e6add0fafb4596af2fc68a50d74bf779b3dac3d225ff24da4b6bd4c70"
   license "MIT"
 
