d001e82bc0b99c7152f8007037cbf097fd92c306
vandevsysX64 <vandevdev.com@gmail.com>
2026-03-03 13:51:24 +0400

    Update sha256 checksum for lol.rb

--- Formula/lol.rb
+++ Formula/lol.rb
@@ -2,7 +2,7 @@ class Lol < Formula
   desc "Rainbow fortune CLI"
   homepage "https://github.com/vandevsysX64/homebrew-lol"
   url "https://github.com/vandevsysX64/homebrew-lol/archive/refs/tags/v1.0.tar.gz"
-  sha256 "TEMP_SHA"
+  sha256 "cbaff96e6add0fafb4596af2fc68a50d74bf779b3dac3d225ff24da4b6bd4c70"
   license "CC-BY-NC-4.0"
 
   depends_on "fortune"
